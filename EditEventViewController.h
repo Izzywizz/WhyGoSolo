e302050972b89ea43359811d0140bf8f1e23f6d4
Izzywizz <izzywizzz@gmail.com>
2016-09-29 14:09:56 +0100

    Finished adding previous components from the create event so they are now part of the edit view controller

--- EditEventViewController.h
+++ EditEventViewController.h
@@ -9,10 +9,15 @@
 #import <UIKit/UIKit.h>
 #import "FontSetup.h"
 #import "ISEmojiView.h"
+#import "CustomerCollectionViewCell.h"
+#import "HeaderCollectionReusableView.h"
 
 
 @interface EditEventViewController : UIViewController
+
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *saveButton;
 @property (weak, nonatomic) IBOutlet UITextView *eventDescriptionInput;
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (weak, nonatomic) IBOutlet UILabel *publicPrivateLabel;
 
 @end
--- EditEventViewController.m
+++ EditEventViewController.m
@@ -15,7 +15,12 @@
 
 
 @property NSString *placeholderEventText;
+@property BOOL isPrivateEvent;
 
+@property NSString *privateText;
+@property NSString *publicText;
+@property NSArray *dummyData;
+@property NSArray *sectionTitles;
 @end
 
 @implementation EditEventViewController
@@ -24,11 +29,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self createDummyData];
     [self navigationButtonSetup];
     [self previousEventTextViewSetup];
     
     
-    //This text is activated when the user hsa no text within their, so they bascially delete all their text.
+    //Register The Nib for the collection cell
+    [self.collectionView registerNib:[UINib nibWithNibName:@"CollectionCell" bundle:nil] forCellWithReuseIdentifier:@"Cell"];
+    self.collectionView.backgroundColor = [UIColor clearColor];
+    
+    //Add padding betweeen the section headers
+    UICollectionViewFlowLayout *collectionViewLayout = (UICollectionViewFlowLayout*)self.collectionView.collectionViewLayout;
+    collectionViewLayout.sectionInset = UIEdgeInsetsMake(20, 0, 20, 0);
+    
+    //Setting up the labels within the collection view
+    _privateText = @"PRIVATE EVENT";
+    _publicText = @"PUBLIC EVENT";
+    
+    //Intially private mode is off thus the tableView has been hidden
+    self.collectionView.hidden = YES;
+    
+    //create circular profile view
+    _circularView.layer.cornerRadius = _circularView.bounds.size.width/2;
+    _circularView.layer.masksToBounds = YES;
+    
+    //This text is activated when the user has no text within their event, so they bascially delete all their text.
     _placeholderEventText = @"Describe your event in less than 140 characters, don’t forget to include the time....";
     
     //create circular emoji view
@@ -42,6 +67,55 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - CollectionView Methods
+
+-(void) createDummyData {
+    
+    // Initialize recipe image array
+    NSArray *mainDishImages = [NSArray arrayWithObjects:@"egg_benedict.jpg", @"full_breakfast.jpg", @"ham_and_cheese_panini.jpg", @"ham_and_egg_sandwich.jpg", @"hamburger.jpg", @"instant_noodle_with_egg.jpg", @"japanese_noodle_with_pork.jpg", @"mushroom_risotto.jpg", @"noodle_with_bbq_pork.jpg", @"thai_shrimp_cake.jpg", @"vegetable_curry.jpg", nil];
+    NSArray *drinkDessertImages = [NSArray arrayWithObjects:@"angry_birds_cake.jpg", @"creme_brelee.jpg", @"green_tea.jpg", @"starbucks_coffee.jpg", @"white_chocolate_donut.jpg", nil];
+    self.dummyData = [NSArray arrayWithObjects:mainDishImages, drinkDessertImages, nil];
+    
+    _sectionTitles = [[NSArray alloc] init];
+    _sectionTitles = @[@"FRIENDS", @"EVERYONE"];
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return [[self.dummyData objectAtIndex:section] count];
+}
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return [self.dummyData count]; //2
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    
+    static NSString *identifier = @"Cell";
+    
+    CustomerCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:identifier forIndexPath:indexPath];
+    
+    cell.profileImageView.image = [UIImage imageNamed:[self.dummyData[indexPath.section] objectAtIndex:indexPath.row]];
+    
+    return cell;
+    
+}
+
+- (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionReusableView *reusableview = nil;
+    
+    if (kind == UICollectionElementKindSectionHeader) {
+        HeaderCollectionReusableView *headerView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"HeaderCell" forIndexPath:indexPath];
+        headerView.sectionHeader.text = [_sectionTitles objectAtIndex:indexPath.section];
+        
+        reusableview = headerView;
+    }
+    
+    return reusableview;
+}
+
 
 #pragma mark - Helper Methods
 
@@ -61,6 +135,21 @@
 }
 
 #pragma mark - ACtion Methods
+
+- (IBAction)eventSwitch:(UISwitch *)sender {
+    if (sender.on) {
+        NSLog(@"Activate Privacy");
+        self.collectionView.hidden = NO;
+        _isPrivateEvent = YES;
+        self.publicPrivateLabel.text = self.privateText;
+    } else  {
+        NSLog(@"Public Mode, Hide TableView");
+        self.collectionView.hidden = YES;
+        _isPrivateEvent = NO;
+        self.publicPrivateLabel.text = self.publicText;
+        
+    }
+}
 
 - (IBAction)backButtonPressed:(UIBarButtonItem *)sender {
     [self.navigationController popViewControllerAnimated:YES];
