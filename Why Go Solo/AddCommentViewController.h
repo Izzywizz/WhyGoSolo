3cb23552a1c212688c685b5e1d6dddc9a386af96
Izzywizz <izzywizzz@gmail.com>
2016-10-18 08:53:36 +0100

    *fixed an issue with comments going off screen, it was due to constraints.

--- Why Go Solo/AddCommentViewController.h
+++ Why Go Solo/AddCommentViewController.h
@@ -14,5 +14,6 @@
 @property (nonatomic) NSString *userComments;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
 @property (weak, nonatomic) IBOutlet UIButton *sendButton;
+@property (weak, nonatomic) IBOutlet UILabel *charCount;
 
 @end
